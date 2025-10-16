a50bc4fdec0d0b938862a2b9fd7fa0cce90d7037
bridge-22 <hycker14@gmail.com>
2025-10-16 09:35:48 +0500

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -2,4 +2,5 @@
 apt-get install -y gcc python3-devel make libsqlite3-devel
 apt-get install python3 python3-module-pip python3-venv curl wget build-essential libssl-dev libffi-dev python3-dev net-tools sqlite3 -y
 pip3 install -r requirements.txt
-python3 bot_main.py
+python3 bot_main.py &
+python3 admin_panel.py &
