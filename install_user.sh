4b5ea51f7151c7034cdda86ff7f9377224ca3575
bridge-22 <hycker14@gmail.com>
2025-10-16 09:42:41 +0500

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-apt-get install -y gcc python3-devel make libsqlite3-devel
+apt-get install -y gcc python3-devel make libsqlite3-devel git
 apt-get install python3 python3-module-pip python3-venv curl wget build-essential libssl-dev libffi-dev python3-dev net-tools sqlite3 -y
+apt-get install -y tmux
 pip3 install -r requirements.txt
-python3 bot_main.py &
-python3 admin_panel.py &
+git clone https://github.com/bridge-22/Telegram-bot.git
+cd
+cd Telegram-bot/
+tmux new-session -d -s myscripts 'python3 bot_main.py'
+tmux new-window -t myscripts 'python3 admin_panel.py'
+echo "Switch between scripts: Ctrl+b n (next window) / Ctrl+b p (previous window)"
+echo "See list of windows: Ctrl+b w"
+echo "Go back to background: Ctrl+b d (detach - scripts keep running)"
+sleep 2
+tmux attach-session -t myscripts
+tmux list-sessions
