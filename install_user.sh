1048a146712c40631e9f304d228712b6bb1ef54d
bridge-22 <hycker14@gmail.com>
2025-10-20 16:18:19 +0500

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -8,6 +8,7 @@ git clone https://github.com/bridge-22/Telegram-bot.git
 cd Telegram-bot/
 tmux new-session -d -s myscripts 'python3 bot_main.py'
 tmux new-window -t myscripts 'python3 admin_panel.py'
+echo "To show scripts console: tmux attach -t myscripts"
 echo "Switch between scripts: Ctrl+b n (next window) / Ctrl+b p (previous window)"
 echo "See list of windows: Ctrl+b w"
 echo "Go back to background: Ctrl+b d (detach - scripts keep running)"
