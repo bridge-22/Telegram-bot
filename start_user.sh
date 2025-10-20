6ea1c2d750081b7c86372494cf77e782e9766028
bridge-22 <hycker14@gmail.com>
2025-10-20 16:42:35 +0500

    Update start_user.sh

--- start_user.sh
+++ start_user.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 path_to_bot="Telegram-bot"
 
-while [[ ! -d $path_to_bot ]]; do
- echo "Folder doesn't exists, write a path to it, or use git clone https://github.com/bridge-22/Telegram-bot"
- read -p "path_to_bot=" path_to_bot
-done
-
-echo "path_to_bot=$path_to_bot"
-cd $path_to_bot
-pwd
-
 tmux new-session -d -s myscripts 'python3 bot_main.py'
 tmux new-window -t myscripts 'python3 admin_panel.py'
 echo "To show scripts console: tmux attach -t myscripts"
