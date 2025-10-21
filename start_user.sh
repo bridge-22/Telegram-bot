3456e1be11b3cf97be3121186d7bab2b42b2e8e1
bridge-22 <hycker14@gmail.com>
2025-10-21 11:44:27 +0500

    Update start_user.sh

--- start_user.sh
+++ start_user.sh
@@ -3,9 +3,9 @@ source .env
 echo "$CLONE_PATH"
 cd $CLONE_PATH
 cd Telegram-bot/
-tmux new-session -d -s myscripts 'python3 bot_main.py'
-tmux new-window -t myscripts 'python3 admin_panel.py'
-echo "To show scripts console: tmux attach -t myscripts"
+tmux new-session -d -s tgbot 'python3 bot_main.py'
+tmux new-window -t tgbot 'python3 admin_panel.py'
+echo "To show scripts console: tmux attach -t tgbot"
 echo "Switch between scripts: Ctrl+b n (next window) / Ctrl+b p (previous window)"
 echo "See list of windows: Ctrl+b w"
 echo "Go back to background: Ctrl+b d (detach - scripts keep running)"
